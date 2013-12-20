c19a03c3f99da60f410bb07f532aecf3e14b76fc
carpy1918 <curtis@carpy.net>
2013-12-19 21:07:30 -0600

    loop w augeas example

--- puppet/modules/hosts/manifests/process.pp
+++ puppet/modules/hosts/manifests/process.pp
@@ -1,31 +1,30 @@
-
 class process {
 
-$file = '/etc/hosts'
-$datahash = { 	
-		'127.0.0.1' => 'localhos',
-		'192.168.85.254' => 'mngtsvr.carpy.net'
+#
+#data values for /etc/ssh/sshd_config
+#
+$datahash = {
+
+                'X11Forwarding' => 'no',
+                'blah' => 'yes'
+
+
 } #end hash
 
-#$datahash.each |$key, $value| { notify{ $key: message => "$key $value" } } #end hash
-$datahash.each |$key, $value| { 
 
-augeas { $key: 
+
+$file = '/files/etc/ssh/sshd_config'
+each($datahash) |$key, $value| {
+
+augeas { $key:
    name => $key,
-   context => "/files/etc/hosts",
-   changes => "set *[ipaddr = $key]/canonical $value",		#[set|rm|clear] <VALUE> <PATH>, or ARRAY of commands
-   load_path => "/usr/share/augeas/lenses/"
-#   onlyif => "match $value not_include $file",
- } #end type
-} #end each
+   context => "$file",
+   changes => "set $file/$key $value",          #[set|rm|clear] <VALUE> <PATH>
+   load_path => "/usr/share/augeas/lenses/",
+#   onlyif => "match $file/$key not_include disable",
+} #end type
 
-augeas { blah: 
-   name => 'blah',
-   context => '/files/etc/hosts',
-   changes => [ "set ipaddr 192.168.85.111",
-		"set canonical blah" ],		#[set|rm|clear] <VALUE> <PATH>, or ARRAY of commands
-   load_path => "/usr/share/augeas/lenses/"
- } #end type
+} #end each
 
 } #end class
 
