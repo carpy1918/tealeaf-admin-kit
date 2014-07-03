56746d110eed69b3cb10a041713b6fca5fe7a651
Curtis Carpenter <curtis@tealeafit.com>
2014-07-03 05:10:27 -0700

    pam.d work and report mod

--- scripts/tightening/pam-cracklib.sh
+++ scripts/tightening/pam-cracklib.sh
@@ -6,16 +6,20 @@
 
 . /home/tealeaf/tealeaf-admin-kit/scripts/tealeaf-env.sh
 
+for file in system-auth password-auth;do
+
 if [ "$MODE" = "MONITOR" ]; then
-  value=`grep cracklib /etc/pam.d/*`
+  value=`grep cracklib /etc/pam.d/$file`
   if [ "$value" = "" ]; then
-    prt "pam-cracklib: no entries found"
+    prt "PAM-CRACKLIB: no entries found"
   else
-    prt "pam-cracklib: $value"
+    prt "PAM-CRACKLIB: $value"
   fi
 else
   sed -r -i 's/password\s+requisite\s+pam_cracklib.so.*/password   requisite   pam_cracklib.so try_first_pass retry=3 minlen=12 dcredit=1 ucredit=1 ocredit=1 lcredit=1 difok=3/' /etc/pam.d/system-auth 
-  prt "pam-cracklib: modifiection complete"
-  value=`grep cracklib /etc/pam.d/*`
-  prt "pam-cracklib: $value" 
+c  prt "PAM-CRACKLIB: modifiection complete"
+  value=`grep cracklib /etc/pam.d/$file`
+  prt "PAM-CRACKLIB: $value" 
 fi
+
+done
