bf9796359e7150708684c909ecfd827926d89fd3
Curtis Carpenter <curtis@tealeafit.com>
2014-07-03 01:57:09 -0700

    report generation per node

--- scripts/tightening/enable-su-auth.sh
+++ scripts/tightening/enable-su-auth.sh
@@ -10,13 +10,13 @@ VALUE=`grep pam_wheel /etc/pam.d/su`
 
 if [ "$MODE" = "EXECUTE" ]; then
   sed -i 's/#auth\s+required\s+pam_wheel.so\s+use_uid/auth   required   pam_wheel.so use_uid/' /etc/pam.d/su
-  prt "enable-su-auth: $MODE: pam_wheel.so (wheel group, dummy) is enabled"
-  prt "enable-su-auth: $MODE: $VALUE"
+  prt "ENABLE-SU-AUTH: $MODE: pam_wheel.so (wheel group, dummy) is enabled"
+  prt "ENABLE-SU-AUTH: $MODE: $VALUE"
 
   SUUSERS=`grep wheel: /etc/group`
   for i in "${SUAUTH[@]}";do
     if [[ "$SUUSERS" =~ $i ]]; then
-      prt "enable-su-auth: $MODE: $i found in wheel group"
+      prt "ENABLE-SU-AUTH: $MODE: $i found in wheel group"
     else
       if [ ! `sed -i -r -e "{/wheel:x:..:$/ s/:$/:$i/}" /etc/group` ];then
  	echo "adding $i to wheel"
@@ -25,6 +25,6 @@ if [ "$MODE" = "EXECUTE" ]; then
     fi
   done
 else
-  prt "enable-su-auth: $MODE: $VALUE"
+  prt "ENABLE-SU-AUTH: $MODE: $VALUE"
 fi
 
