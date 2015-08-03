59a24f05baf7af1b60f6492c95444b54f23b25ea
carpy1918 <curtis@carpy.net>
2015-08-03 17:03:38 -0500

    login.def security tightening

--- puppet/modules/login/init.pp
+++ puppet/modules/login/init.pp
@@ -2,19 +2,19 @@ class login::conf {
 
 file { "login":
   name => $operatingsystem ? {
-  default => "/etc/ssh/login",
+  default => "/etc/login.defs",
    },
 }
 
- exec { "sshdconfig":
-   command => "/etc/init.d/sshd reload",
-   alias       => "sshdconfig",
-   refreshonly => true,
-   subscribe   => File["login"],
-}
+# exec { "login":
+#   command => "/etc/init.d/ reload",
+#   alias       => "sshdconfig",
+#   refreshonly => true,
+#   subscribe   => File["login"],
+#}
 
 define login::mod ( $key, $value ) {
- $context = "/files/etc/ssh/login"
+ $context = "/files/etc/login.defs"
 
  warning("login::mod with $key and $value and $name")
 
@@ -29,52 +29,88 @@ define login::mod ( $key, $value ) {
 
 login::mod { 'mod1': 
   name => 'login-mod1',
-  key => 'ClientAliveInterval',
-  value => '3000',
+  key => 'MAIL_DIR',
+  value => '/var/spool/mail',
  }
 
 login::mod { 'mod2': 
   name => 'login-mod2',
-  key => 'ClientAliveCountMax',
-  value => '10',
+  key => 'MAIL_FILE',
+  value => '.mail',
  }
 
 login::mod { 'mod3': 
   name => 'login-mod3',
-  key => 'HostbasedAuthentication',
-  value => 'no',
+  key => 'PASS_MAX_DAYS',
+  value => '180',
  }
 
 login::mod { 'mod4': 
   name => 'login-mod4',
-  key => 'PermitRootLogin',
-  value => 'no',
+  key => 'PASS_MIN_DAYS',
+  value => '1',
  }
 
  login::mod { 'mod5':
    name => 'login-mod5',
-   key => 'PermitEmptyPasswords',
-   value => 'no',
+   key => 'PASS_MIN_LEN',
+   value => '12',
   }
 
  login::mod { 'mod6':
    name => 'login-mod6',
-   key => 'Protocol',
-   value => '2',
+   key => 'PASS_WARN_AGE',
+   value => '14',
   }
 
  login::mod { 'mod7':
    name => 'login-mod7',
-   key => 'Banner',
-   value => '/etc/issue',
+   key => 'UID_MIN',
+   value => '500',
   }
 
  login::mod { 'mod8':
    name => 'login-mod8',
-   key => 'Ciphers',
-   value => 'aes256-cbc,blowfish-cbc',
+   key => 'UID_MAX',
+   value => '60000',
   }
 
-} #end sysctl::conf
+ login::mod { 'mod9':
+   name => 'login-mod9',
+   key => 'GID_MIN',
+   value => '500',
+  }
+
+login::mod { 'mod10':
+name => 'login-mod10',
+key => 'GID_MAX',
+value => '60000',
+}
+
+login::mod { 'mod11':
+name => 'login-mod11',
+key => 'REATE_HOME',
+value => 'yes',
+}
+
+login::mod { 'mod12':
+name => 'login-mod12',
+key => 'UMASK',
+value => '077',
+}
+
+login::mod { 'mod13':
+name => 'login-mod13',
+key => 'USERGROUPS_ENAB',
+value => 'yes',
+}
+
+login::mod { 'mod14':
+name => 'login-mod14',
+key => 'ENCRYPT_METHOD',
+value => 'SHA512',
+}
+
+} #end login::conf
 
 include login::conf
