c3e74ce6569a71d419eeed7277591f2094024843
carpy1918 <curtis@carpy.net>
2015-08-03 16:48:29 -0500

    mod

--- puppet/modules/login/init.pp
+++ puppet/modules/login/init.pp
@@ -1,8 +1,8 @@
-class sshd_config::conf {
+class login::conf {
 
-file { "sshd_config":
+file { "login":
   name => $operatingsystem ? {
-  default => "/etc/ssh/sshd_config",
+  default => "/etc/ssh/login",
    },
 }
 
@@ -10,13 +10,13 @@ file { "sshd_config":
    command => "/etc/init.d/sshd reload",
    alias       => "sshdconfig",
    refreshonly => true,
-   subscribe   => File["sshd_config"],
+   subscribe   => File["login"],
 }
 
-define sshd_config::mod ( $key, $value ) {
- $context = "/files/etc/ssh/sshd_config"
+define login::mod ( $key, $value ) {
+ $context = "/files/etc/ssh/login"
 
- warning("sshd_config::mod with $key and $value and $name")
+ warning("login::mod with $key and $value and $name")
 
  augeas { "$name":
    name => $name,
@@ -27,54 +27,54 @@ define sshd_config::mod ( $key, $value ) {
   }
 } #end sysctl::mod
 
-sshd_config::mod { 'mod1': 
-  name => 'sshd_config-mod1',
+login::mod { 'mod1': 
+  name => 'login-mod1',
   key => 'ClientAliveInterval',
   value => '3000',
  }
 
-sshd_config::mod { 'mod2': 
-  name => 'sshd_config-mod2',
+login::mod { 'mod2': 
+  name => 'login-mod2',
   key => 'ClientAliveCountMax',
   value => '10',
  }
 
-sshd_config::mod { 'mod3': 
-  name => 'sshd_config-mod3',
+login::mod { 'mod3': 
+  name => 'login-mod3',
   key => 'HostbasedAuthentication',
   value => 'no',
  }
 
-sshd_config::mod { 'mod4': 
-  name => 'sshd_config-mod4',
+login::mod { 'mod4': 
+  name => 'login-mod4',
   key => 'PermitRootLogin',
   value => 'no',
  }
 
- sshd_config::mod { 'mod5':
-   name => 'sshd_config-mod5',
+ login::mod { 'mod5':
+   name => 'login-mod5',
    key => 'PermitEmptyPasswords',
    value => 'no',
   }
 
- sshd_config::mod { 'mod6':
-   name => 'sshd_config-mod6',
+ login::mod { 'mod6':
+   name => 'login-mod6',
    key => 'Protocol',
    value => '2',
   }
 
- sshd_config::mod { 'mod7':
-   name => 'sshd_config-mod7',
+ login::mod { 'mod7':
+   name => 'login-mod7',
    key => 'Banner',
    value => '/etc/issue',
   }
 
- sshd_config::mod { 'mod8':
-   name => 'sshd_config-mod8',
+ login::mod { 'mod8':
+   name => 'login-mod8',
    key => 'Ciphers',
    value => 'aes256-cbc,blowfish-cbc',
   }
 
-} #end sshd_config::conf
+} #end sysctl::conf
 
-include sshd_config::conf
+include login::conf
